c661b1392e3ca28a6b0e5e533359ab3f341aa61d
Irineu Aparecido Amigo <ra2457078>
2026-05-20 21:32:22 -0300

    mudança do botão torcar fotos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,12 +7,12 @@ import * as ImagePicker from 'expo-image-picker';
 import { useState } from "react";
 
 
-
 const PlaceholderImage = require('@/assets/images/natacao2.png'); 
 
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -23,6 +23,7 @@ export default function Index() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
 
     } else { 
       alert('Nenhuma imagem selecionada');
@@ -35,16 +36,19 @@ export default function Index() {
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage}/>
         
       </View>
+      {showAppOptions ? (
+          <View />
+  ) : (
       <View style={styles.footerContainer}>
         <Button theme="primary" label = "Escolha uma foto" onPress={pickImage} />
-        <Button label="Use esta foto" />
-      </View>
+        <Button label="Use esta foto" onPress={() => setShowAppOptions(true)} />      
+        </View>
+      )}  
     </View>
   );
 }
   
 
- 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
